e1ad220d7618b34fe6e8690ad01d481cbc221651
subtainishfaq <Bese-18a-+>
2016-10-11 03:50:27 +0500

    Changes

--- app/app.js
+++ app/app.js
@@ -5,7 +5,9 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'angularFileUpload'
+
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
