151c66208e0faadf03325f420868b35d18c0e8aa
subtainishfaqstudent <subtainishfaq@Subtains-MacBook-Pro.local>
2016-10-16 01:35:57 +0500

    upload form to upload repo

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -6,17 +6,22 @@ angular.module('myApp.view2', ['ngRoute'])
 .controller('View2Ctrl', ['$scope','$state','repofilelist',function($scope,$state,repofilelist)
 {
   console.log(repofilelist);
-  $scope.photos = repofilelist.data;/*[
-    {id: 'p1', 'title': 'A nice day!', src: "http://lorempixel.com/300/400/"},
-    {id: 'p2', 'title': 'Puh!', src: "http://lorempixel.com/300/400/sports"},
-    {id: 'p3', 'title': 'What a club!', src: "http://lorempixel.com/300/400/nightlife"},
-    {id: 'p1', 'title': 'A nice day!', src: "http://lorempixel.com/300/400/"},
-    {id: 'p2', 'title': 'Puh!', src: "http://lorempixel.com/300/400/sports"},
-    {id: 'p3', 'title': 'What a club!', src: "http://lorempixel.com/300/400/nightlife"}
-  ];*/
+  $scope.photos = repofilelist.data;
+  $scope.isUploadForm=false;
+
+  if (repofilelist.data.length==0)
+  {
+
+  }
   $scope.doSomething=function ()
   {
     $state.go("edit");
-  }
+  };
+  $scope.showUploadForm=function ()
+  {
+    $scope.isUploadForm=true;
+  };
+
+
 
 }]);
